2282786aba2b803408507d118c9508cc1cf2933f
Mez-A <dimoabr@gmail.com>
2022-06-04 10:01:20 +0200

    prepared translator as a context

--- zemosa/src/context/AppContext.jsx
+++ zemosa/src/context/AppContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const AppContext = createContext();
-// 
+
+// APIs for getting and translating languages
 const languagesUrl = "https://libretranslate.de/languages";
 const translateUrl = "https://libretranslate.de/translate";
 
@@ -13,6 +14,7 @@ export const AppProvider = ({ children }) => {
     const [text, setText] = useState("prüfung");
     const [translatedText, setTranslatedText] = useState("");
 
+    // we are putting the list of languages we get from the API into the state variable of [languages, setLanguages]
     useEffect(() => {
         (async () => {
             setLanguages((await axios.get(languagesUrl)).data, {
@@ -21,6 +23,7 @@ export const AppProvider = ({ children }) => {
         })();
     }, []);
 
+    //This is a function for translating the language
     const translate = async () => {
         const params = new URLSearchParams();
         params.append("q", text);
@@ -29,6 +32,7 @@ export const AppProvider = ({ children }) => {
         params.append("format", "text");
         params.append("api_key", "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx");
 
+        // We set the translated text into the state variable of [translatedText, setTranslatedText]
         setTranslatedText(
             (
                 await axios.post(translateUrl, params, {
