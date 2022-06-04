1d16e8d4b9c2ff0a6ca47c3bc08de5a2527b8fa0
Mez-A <dimoabr@gmail.com>
2022-06-04 11:57:54 +0200

    adjustments to login form

--- zemosa/src/App.jsx
+++ zemosa/src/App.jsx
@@ -1,27 +1,15 @@
-
-
-
-
-
-
-
-    
-      
-   
-
 // import {NavLink, Route, Routes} from 'react-router-dom';
-  import Login from  './components/Login'
+import Login from "./components/Login";
 import { Test } from "./components/Test";
 import "./App.scss";
 
 function App() {
     return (
         <div className="App">
-      <Login />
+            <Login />
             <Test />
         </div>
     );
-
 }
 
 export default App;
