84e168630a8ca07403a05da57eaafa7b87dadceb
Mez-A <dimoabr@gmail.com>
2022-06-04 11:54:39 +0200

    adjustments to login form

--- zemosa/src/App.jsx
+++ zemosa/src/App.jsx
@@ -1,10 +1,12 @@
 // import {NavLink, Route, Routes} from 'react-router-dom';
+import Login from "./components/Login";
 import { Test } from "./components/Test";
 import "./App.scss";
 
 function App() {
     return (
         <div className="App">
+            <Login />
             <Test />
         </div>
     );
---
+++ zemosa/src/components/Login.jsx
@@ -0,0 +1,113 @@
+import { useRef, useState, useEffect } from "react";
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [user, pwd]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(
+                LOGIN_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { "Content-Type": "application/json" },
+                    withCredentials: true,
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response?.data));
+            const accessToken = response?.data.accessToken;
+            const roles = response?.data?.response?.data.roles;
+            setAuth({ user, pwd, roles, accessToken });
+
+            setUser("");
+            setPwd("");
+            setSuccess(true);
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg("No Server Response");
+            } else if (err.response?.status === 400) {
+                setErrMsg("missing Username or password");
+            } else if (err.response?.status === 401) {
+                setErrMsg("Unauthorized");
+            } else {
+                setErrMsg("login failed");
+            }
+            errRef.current.focus();
+        }
+    };
+
+    return (
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a hrf="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+                <section>
+                    <p
+                        ref={errRef}
+                        className={errMsg ? "errmsg" : "offscreen"}
+                        aria-live="assertive"
+                    >
+                        {errMsg}
+                    </p>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="username">Username:</label>
+
+                        <input
+                            type="text"
+                            id="username"
+                            ref={userRef}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                        />
+
+                        <label htmlFor="password">Password:</label>
+
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(e) => setPwd(e.target.value)}
+                            value={pwd}
+                            required
+                        />
+
+                        <button>Sign In</button>
+                    </form>
+                    <p>
+                        Need an Account?
+                        <br />
+                        <span className="line">
+                            {/*put router link here*/}
+                            <a hrf="#">Sign Up</a>
+                        </span>
+                    </p>
+                </section>
+            )}
+        </>
+    );
+};
+export default Login;
