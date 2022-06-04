033ebc104a4e6369c3fcc61958f2d4bdf96694be
AN-SZ <90573297+AN-SZ@users.noreply.github.com>
2022-06-04 11:28:27 +0200

    Anwer: Login finished

--- zemosa/src/App.jsx
+++ zemosa/src/App.jsx
@@ -1,13 +1,27 @@
+
+
+
+
+
+
+
+    
+      
+   
+
 // import {NavLink, Route, Routes} from 'react-router-dom';
+  import Login from  './components/Login'
 import { Test } from "./components/Test";
 import "./App.scss";
 
 function App() {
     return (
         <div className="App">
+      <Login />
             <Test />
         </div>
     );
+
 }
 
 export default App;
