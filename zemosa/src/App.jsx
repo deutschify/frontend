db92ac15a7d7680fdf13a8494f526069314fb643
Zubia <rashzubia@gmail.com>
2022-06-06 14:51:01 +0200

    zubia-aktuellerStatus

--- zemosa/src/App.jsx
+++ zemosa/src/App.jsx
@@ -2,13 +2,17 @@
 // import Login from "./components/Login";
 import {Register} from "./components/Register";
 import { Test } from "./components/Test";
+import Search from './components/einbuergerungstest'
+// import Einbuergerungstest from "./components/einbuergerungstest"
 import "./App.scss";
 
 function App() {
     return (
         <div className="App">
             <Register/>
+            <Search/>
             {/* <Test /> */}
+            
         </div>
     );
 }
---
+++ zemosa/src/components/einbuergerungstest.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import '../style/App.scss';
+import QuestionDE from '../../../../backend/einbuergerungstestJSON'
+
+const Search = () => {
+    const [query, setQuery] = useState('');
+
+    return ( 
+        <div className="searchBar">
+            <h1>Einbürgerungstestfragen</h1>
+            <input type="text" placeholder="search..." onChange={e=>setQuery(e.target.value)} />
+            {QuestionDE.filter((testQuestion) => {
+                if(query ==''){
+                    return testQuestion
+                }
+                else if (testQuestion.question.toLowerCase().includes(query.toLowerCase())){
+                    return testQuestion;
+                }
+                else if (testQuestion.number.includes(query)){
+                    return testQuestion;
+                }
+            }).map((testQuestion) =>{
+                return <div key={testQuestion.number}  >{testQuestion.number}: {''}{testQuestion.question}</div>;
+            })}
+        </div>
+     );
+}
+ 
+export default Search;
