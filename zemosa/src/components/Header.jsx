47aa60dd27746bb9989019110686c06a3fb08916
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-06-07 09:20:40 +0200

    added hamburgerIcon to the header

--- zemosa/src/components/Header.jsx
+++ zemosa/src/components/Header.jsx
@@ -1,11 +1,30 @@
+import React, { useState } from "react";
+import { HamburgerIcon } from "react-hamburger-icon";
 const Header = () => {
+    const [open, setOpen] = useState(false);
     const headerStyles = {
         backgroundColor: "#ddd",
         color: "red",
         fontSize: "1.6rem",
         textAlign: "center",
+        position: "relative",
     };
-    return <div style={headerStyles}>Header</div>;
+    const burgerStyles = {
+        position: "absolute",
+        top: "0.5%",
+        left: "0.5%",
+    };
+    return (
+        <>
+            <div style={headerStyles}>Header</div>
+
+            <HamburgerIcon
+                style={burgerStyles}
+                open={open}
+                onClick={() => setOpen(!open)}
+            />
+        </>
+    );
 };
 
 export default Header;
